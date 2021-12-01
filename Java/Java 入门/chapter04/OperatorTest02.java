51325a2d5ca48ffcc9f51007dabcce0f07dac017
cuiweizhi <330459539@qq.com>
2021-12-01 11:01:55 +0800

    Add a new file: OperatorTest02.java

---
+++ Java/Java 入门/chapter04/OperatorTest02.java
@@ -0,0 +1,22 @@
+/*
+* @Author: StudentCWZ
+* @Date:   2020-06-09 13:14:06
+* @Last Modified by:   StudentCWZ
+* @Last Modified time: 2020-06-09 13:18:17
+*/
+
+
+public class OperatorTest02
+{
+  public static void main(String[] args){
+    int a = 10;
+    int b = 10;
+    System.out.println(a > b);//false
+    System.out.println(a >= b);//true
+    System.out.println(a < b);//false
+    System.out.println(a <= b);//false
+    System.out.println(a == b);//true
+    System.out.println(a != b);//false
+  }
+}
+
